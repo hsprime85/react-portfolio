d66801e43e11b9e1541caadca9eb8d769826aab8
hsprime85 <hsprime85@gmail.com>
2021-04-14 17:06:28 -0700

    nav bar and header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Particles from 'react-particles-js'
+import NavbarComponent from './components/Navbar.component'
+import Header from './components/Header.component'
 
 function App() {
   return (
-    <h1>portfolio</h1>
+    <>
+      <Particles className='particles-canvas'
+        params={{ 
+          particles:{ 
+            number: {
+              value: 20,
+              density:{
+                enable: true,
+                value_area: 700
+              }
+            },
+            shape:{
+              type: 'circle',
+              stroke:{
+                width: 6,
+                color: '#f9ab00'
+              }
+            }
+          }
+        }}
+      />
+      <NavbarComponent />
+      <Header/>
+    </>
+
   );
 }
 
---
+++ src/components/Navbar.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Navbar, Nav } from 'react-bootstrap'
+import logo from '../logo.png'
+
+const NavbarComponent = () => {
+    return (
+        <Navbar bg="light" expand="lg">
+            <div className='container'>
+                <Navbar.Brand href="#home"><img className='logo' src={logo} alt='logo'/></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link className='nav-link mr-5' href="#home">Home</Nav.Link>
+                        <Nav.Link className='nav-link mr-5' href="#aboutme">About me</Nav.Link>
+                        <Nav.Link className='nav-link mr-5' href="#services">Services</Nav.Link>
+                        <Nav.Link className='nav-link mr-5' href="#howwork">how work</Nav.Link>
+                        <Nav.Link className='nav-link mr-5' href="#portfolios">portfolios</Nav.Link>
+                        <Nav.Link className='nav-link mr-5' href="#contact">contact</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </div>
+        </Navbar>
+    )
+}
+
+export default NavbarComponent
